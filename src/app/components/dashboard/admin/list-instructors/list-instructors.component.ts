c36599df8bbb1c5424703d87aa38ebe34cb93470
ghozziayoub <ghozziayoub@gmail.com>
2020-06-21 10:49:14 +0100

    update state account

--- src/app/components/dashboard/admin/list-instructors/list-instructors.component.ts
+++ src/app/components/dashboard/admin/list-instructors/list-instructors.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { InstructorService } from 'src/app/services/instructor.service';
 
 @Component({
   selector: 'app-list-instructors',
   templateUrl: './list-instructors.component.html',
-  styleUrls: ['./list-instructors.component.scss','./../../tableau-bord/tableau-bord.component.scss']
+  styleUrls: ['./list-instructors.component.scss', './../../tableau-bord/tableau-bord.component.scss']
 })
 export class ListInstructorsComponent implements OnInit {
 
-  constructor() { }
+  instructorsList = [];
+
+  constructor(private is: InstructorService) { }
 
   ngOnInit(): void {
+    this.is.allInstructors().subscribe(
+      res => this.instructorsList = res
+    )
   }
+
+  update(id) {
+    this.is.updateAccount(id).subscribe(
+      res => this.ngOnInit()
+    )
+  }
+
+
 
 }
--- src/app/services/instructor.service.ts
+++ src/app/services/instructor.service.ts
@@ -10,6 +10,8 @@ export class InstructorService {
 
   private registerInstructorUrl = "http://localhost:3000/instructor/register";
   private loginInstructorUrl = "http://localhost:3000/instructor/login";
+  private allInstructorsUrl = "http://localhost:3000/instructor/all";
+  private updateAccountInstructorsUrl = "http://localhost:3000/instructor/updateAccountState/";
 
   constructor(private http: HttpClient) { }
 
@@ -20,6 +22,14 @@ export class InstructorService {
   registerInstuctor(instrcutor: Instructor) {
     let dataFromAPI = this.http.post<any>(this.registerInstructorUrl, instrcutor);
     return dataFromAPI;
+  }
+
+  updateAccount(id) {
+    return this.http.patch<any>(this.updateAccountInstructorsUrl + id, null);
+  }
+
+  allInstructors() {
+    return this.http.get<any>(this.allInstructorsUrl);
   }
 
   isLoggedIn() {
